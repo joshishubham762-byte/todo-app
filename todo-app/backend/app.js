285903b1d12538979a4ebb0882138dddd4b4070b
Shubham Joshi <joshishubham762@gmail.com>
2026-08-30 20:09:19 +0530

    small change

--- todo-app/backend/app.js
+++ todo-app/backend/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
@@ -8,32 +9,33 @@ const todoRoutes = require('./routes/todoRoutes');
 const app = express();
 
 const allowedOrigins = [
-  process.env.FRONTEND_ORIGIN,
-  'http://localhost:5173',
-  'http://localhost:4173',
-  'http://localhost:3000',
-  'http://127.0.0.1:5173',
-  'http://127.0.0.1:4173',
+    process.env.FRONTEND_ORIGIN,
+    'http://localhost:5173',
+    'http://localhost:4173',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:4173'
 ].filter(Boolean);
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-      return;
-    }
-
-    callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true,
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            console.log('Blocked CORS origin:', origin);
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
 }));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Todo App API is running');
+    res.send('Todo App API is running');
 });
 
 module.exports = app;
